bdf057c58ac729c6a83695b4488f51b6dfe879ee
pandaman64 <kointosudesuyo@infoseek.jp>
2019-02-18 10:23:24 +0900

    qiskit

--- pkgs/development/python-modules/qiskit/default.nix
+++ pkgs/development/python-modules/qiskit/default.nix
@@ -2,59 +2,33 @@
 , isPy3k
 , buildPythonPackage
 , fetchPypi
-, numpy
-, scipy
-, sympy
-, matplotlib
-, networkx
-, ply
-, pillow
-, cffi
-, requests
-, requests_ntlm
-, IBMQuantumExperience
-, jsonschema
-, psutil
-, cmake
-, llvmPackages 
+, qiskit-aer
+, qiskit-terra
 }:
 
 buildPythonPackage rec {
   pname = "qiskit";
-  version = "0.6.1";
+  version = "0.7.2";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "601e8db4db470593b94a32495c6e90e2db11a9382817a34584520573a7b8cc38";
+    sha256 = "08c7f7ccd32a5cb0c0a0c4f63d6ff068d659c9c0b51e2df6f2054e586e8bfa19";
   };
 
-  buildInputs = [ cmake ]
-    ++ stdenv.lib.optional stdenv.isDarwin llvmPackages.openmp;
-
-  propagatedBuildInputs = [
-    numpy
-    matplotlib
-    networkx
-    ply
-    scipy
-    sympy
-    pillow
-    cffi
-    requests
-    requests_ntlm
-    IBMQuantumExperience
-    jsonschema
-    psutil
+  patches = [
+    ./no-override.patch
   ];
 
-  # Pypi's tarball doesn't contain tests
-  doCheck = false;
+  propagatedBuildInputs = [
+    qiskit-aer
+    qiskit-terra
+  ];
 
   meta = {
     description = "Quantum Software Development Kit for writing quantum computing experiments, programs, and applications";
-    homepage    = https://github.com/QISKit/qiskit-terra;
+    homepage    = https://github.com/Qiskit/qiskit;
     license     = stdenv.lib.licenses.asl20;
     maintainers = with stdenv.lib.maintainers; [
       pandaman
